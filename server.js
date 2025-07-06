2aadeeef015d29f3baa14c84da15db282b95885b
Ian Bonaparte <41701748+ibonaparte@users.noreply.github.com>
2025-07-05 23:46:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,35 @@ const dbFile = "./db/misterbdynasty.db";
 const db = new sqlite.Database(dbFile);
 
 app.use(cors(corsOptions));
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://misterbdynasty-client.onrender.com/"
+  );
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use("/", apiRoutes);
 
 const users = [];
